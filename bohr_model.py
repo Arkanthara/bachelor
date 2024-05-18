6c2a3f5cdb0294915f26e94d283e536b3337fcd8
Fitzwilliam Darcy <fitzwilliam@darcy>
2024-05-18 18:13:55 +0200

    bohr model ok

--- bohr_model.py
+++ bohr_model.py
@@ -3,59 +3,78 @@ import numpy as np
 
 def bohr_model(element: str = 'He', number: int = 2):
 
-    layers = ['g', 'r', 'cyan', 'magenta', 'b', 'orange']
-
+    layers = ['g', 'r', 'cyan', 'magenta', 'b', 'orange', 'yellow']
+    name = ["K", "L", "M", 'N', 'O', 'P', 'Q']
+    sublayer_name = ['s', 'p', 'd', 'f']
+    
     plt.figure()
-
-    plt.title(f"Atom {element}, Z = {number}")
+    plt.title(f"Atome {element}, Z = {number}")
 
     # Plot the kern
     plt.scatter(0, 0, s = 2000)
     plt.text(0, 0, element, horizontalalignment='center', verticalalignment='center')
 
-    steps = [2**i for i in range(10)]
-
     nb_nucleus = number
-    
-    layer = 1
-
     theta = np.linspace(0, 2 * np.pi, 200)
 
-    radius = 0
+    order = ['1s','2s','2p','3s','3p','4s','3d','4p','5s','4d','5p','6s','4f','5d','6p','7s','5f','6d','7p']
+
+    nb_per_layer = {'s': 2, 'p': 6, 'd': 10, 'f': 14}
+    index = 0
+    layer = 1
+    last_layer = 0
+    maxlayer = 0
+    is_saturated = True
 
     # Plot the layers
     while nb_nucleus != 0:
-        
-        layer *= 2
 
-        if layer == 2: layer = 1
-
-        radius += 2
+        layer = int(order[index][0])
+        if layer > maxlayer: maxlayer = layer
+        nb = nb_per_layer[order[index][1]]
+        pad = 0
+        if nb == 6:
+            pad = 0.4
+        if nb == 10:
+            pad = 0.8
+        if nb == 14:
+            pad = 1.2
+        radius = 2 * layer + pad
 
         plt.plot(radius * np.cos(theta), radius * np.sin(theta), 'black', linestyle = ':')
-
-        coordinates = []
         
-        for i in range(layer):
-            # Convert polar coordinates to rectangular coordinates
-            for j in range(2):
-                x = radius * np.cos(2 * np.pi * i / (layer) + (np.pi / (20 * radius)) * (-1)**j)
-                y = radius * np.sin(2 * np.pi * i / (layer) + (np.pi / (20 * radius)) * (-1)**j)
-                coordinates.append((x, y))
-        
-        index = 0
-        while nb_nucleus != 0 and index < layer * 2:
-            
-            plt.plot(coordinates[index][0], coordinates[index][1], color = layers[int(np.log2(layer) - 1)], marker = 'o')
-            index += 1
-            nb_nucleus -= 1
+        for i in range(nb):
+            x = radius * np.cos(2 * np.pi * i / nb)
+            y = radius * np.sin(2 * np.pi * i / nb)
+            if nb_nucleus != 0:
+                plt.plot(x, y, color = layers[layer - 1], marker = 'o')
+                nb_nucleus -= 1
+            else:
+                is_saturated = False
+                last_layer = i
+                break
+        index += 1
+    
+    for i in range(maxlayer - 1):
+        plt.scatter([], [], color=layers[i], label=f"Couche {name[i]}")
 
-        if layer == 1: layer = 2
+    plt.scatter([], [], color=layers[maxlayer - 1], label=f"Couche {name[maxlayer - 1]} (couche de valence)")
 
+    text = "$"
+    for i in range(index - 1):
+        text += order[i] + "^{" + str(nb_per_layer[order[i][1]]) + "}"
+    if is_saturated:
+        text += order[index - 1] + "^{" + str(nb_per_layer[order[index - 1][1]]) + "}$"
+    else:
+        text += order[index - 1] + "^{" + str(last_layer) + "}$"
+
+
+    plt.text(0, -2 * (maxlayer + 1), text, horizontalalignment='center', verticalalignment='center')
     plt.gca().set_aspect(1)
     plt.axis('off')
+    plt.legend()
     plt.show()
 
 
-bohr_model('C', 12)
+bohr_model('Fe', 26)
 
